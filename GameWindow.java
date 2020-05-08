bfe06e73dd2e96137994efa9dac0c58da45f6522
FoxDavenport <54683756+FoxDavenport@users.noreply.github.com>
2020-05-08 10:23:10 -0700

    Add files via upload

---
+++ GameWindow.java
@@ -0,0 +1,222 @@
+package com.adam.game;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.io.IOException;
+import java.net.URL;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class GameWindow
+  extends JPanel
+  implements MissedArrowListener
+{
+  public URL url = GameWindow.class.getResource("/resources/forward.gif");
+  public URL url1 = GameWindow.class.getResource("/resources/backward.gif");
+  public URL url2 = GameWindow.class.getResource("/resources/left.gif");
+  public URL url3 = GameWindow.class.getResource("/resources/right.gif");
+  public URL url4 = GameWindow.class.getResource("/resources/ready.gif");
+  public URL url5 = GameWindow.class.getResource("/resources/basicmiss.gif");
+  public URL url6 = GameWindow.class.getResource("/resources/critmiss.gif");
+  public URL url7 = GameWindow.class.getResource("/resources/death.gif");
+  public URL url8 = GameWindow.class.getResource("/resources/dead.gif");
+  public static URL url9 = GameWindow.class.getResource("/resources/background..jpg");
+  private static final long serialVersionUID = 8121004801717571097L;
+  static JLabel mainWindow;
+  private static JLabel arrowBox;
+  private static ImageIcon forward;
+  private static ImageIcon backward;
+  private static ImageIcon left;
+  private static ImageIcon right;
+  private static ImageIcon ready;
+  private static ImageIcon basicmiss;
+  private static ImageIcon critmiss;
+ 
+  static ImageIcon death;
+  static ImageIcon dead;
+  protected static JFrame frame;
+  protected static ScheduledFuture<?> scheduledFuture;
+  protected static ScheduledFuture<?> scheduledFutureArrowMover;
+  protected static ScheduledExecutorService scheduledExecutorService;
+  protected static ScheduledExecutorService scheduledExecutorServiceArrowMover;
+  protected static AnimatedPanel arrows;
+  public static int x = 1200;
+  protected static long score = 0;
+ 
+  public GameWindow()
+    throws IOException
+  {
+   buildImages();
+    
+    
+    setSize(500, 300);
+    mainWindow = new JLabel(ready);
+
+    arrows = new AnimatedPanel();
+    arrows.addMissedArrowListener(this);
+    arrows.setSize(1200, 100);
+    arrows.setLayout(new FlowLayout(0));
+    arrowBox = new JLabel("");
+    arrowBox.setBounds(0, 0, 80, 80);
+    
+    arrowBox.setPreferredSize(new Dimension(80, 80));
+    arrowBox.setBorder(BorderFactory.createLineBorder(Color.red));
+    arrows.add(arrowBox);
+    setSize(1200, 800);
+    setLayout(new BorderLayout());
+    add(mainWindow, "Center");
+    add(arrows, "South");
+   
+    frame = new JFrame("Dance Dance Revolution");
+    frame.add(this);
+    frame.addKeyListener(new KeyListener()
+    {
+      public void keyPressed(KeyEvent e)
+      {
+        GameWindow.processKey(e.getKeyCode());
+      }
+      
+      public void keyReleased(KeyEvent e) {}
+      
+      public void keyTyped(KeyEvent e) {}
+    });
+    frame.setSize(1200, 800);
+    frame.setDefaultCloseOperation(3);
+    frame.setVisible(true);
+    frame.setResizable(false);
+  }
+  
+  private void buildImages()
+  {
+    forward = new ImageIcon(url);
+    backward = new ImageIcon(url1);
+    left = new ImageIcon(url2);
+    right = new ImageIcon(url3);
+    ready = new ImageIcon(url4);
+    basicmiss = new ImageIcon(url5);
+    critmiss = new ImageIcon(url6);
+    death = new ImageIcon(url7);
+    dead = new ImageIcon(url8);
+
+  }
+  
+  
+  
+  public static void processKey(int key)
+  {
+    try
+    {
+      scheduledFuture.cancel(true);
+    }
+    catch (Exception localException) {}
+    switch (key)
+    {
+    case 37: 
+      if (arrows.checkArrow(1)) {
+        handleHit(left);
+      } else {
+        handleMiss(basicmiss);
+      }
+      break;
+    case 38: 
+      if (arrows.checkArrow(3)) {
+        handleHit(forward);
+      } else {
+        handleMiss(basicmiss);
+      }
+      break;
+    case 39: 
+      if (arrows.checkArrow(0)) {
+        handleHit(right);
+      } else {
+        handleMiss(basicmiss);
+      }
+      break;
+    case 40: 
+      if (arrows.checkArrow(2)) {
+        handleHit(backward);
+      } else {
+        handleMiss(basicmiss);
+      }
+      break;
+    }
+  }
+  
+  private static void handleMiss(ImageIcon missType)
+  {
+    if (AnimatedPanel.totalarrows!= AnimatedPanel.endarrows)
+    {
+      if (missType == critmiss) {
+        AnimatedPanel.totalarrows+= 1;
+      }
+      score -= 5L;
+      
+      mainWindow.setIcon(missType);
+      frame.repaint();
+      scheduledExecutorService = 
+        Executors.newScheduledThreadPool(1);
+      
+      scheduledFuture = 
+        scheduledExecutorService.schedule(new Runnable()
+        {
+          public void run()
+          {
+            GameWindow.mainWindow.setIcon(GameWindow.ready);
+          }
+        }, 1L, 
+        TimeUnit.SECONDS);
+      
+      scheduledExecutorService.shutdown();
+    }
+  }
+  
+  private static void handleHit(ImageIcon moveType)
+  {
+    if (AnimatedPanel.totalarrows != AnimatedPanel.endarrows)
+    {
+      score += 5L;
+      AnimatedPanel.totalarrows += 1;
+      
+      mainWindow.setIcon(moveType);
+    
+	
+      frame.repaint();
+      scheduledExecutorService = 
+        Executors.newScheduledThreadPool(1);
+      
+      scheduledFuture = 
+        scheduledExecutorService.schedule(new Runnable()
+        {
+          public void run()
+          {
+            GameWindow.mainWindow.setIcon(GameWindow.ready);
+          }
+        }, 1L, 
+        TimeUnit.SECONDS);
+      
+      scheduledExecutorService.shutdown();
+    }
+  }
+  
+  public void arrowMissed()
+  {
+    try
+    {
+      scheduledFuture.cancel(true);
+    }
+    catch (Exception localException) {}
+    handleMiss(critmiss);
+  }
+}
---
+++ SettingsWindow.java
@@ -0,0 +1,157 @@
+ package com.adam.game;
+
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.Label;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.URL;
+
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+public class SettingsWindow extends JFrame {
+
+	/**
+	 * 
+	 */
+	
+	private static final long serialVersionUID = 1L;
+	private String[] songs = { "Trololol", "Hampster dance"};
+	private JComboBox<String> c = new JComboBox<String>();
+	private String[] Duration = { "One minute", "Two minutes", "Three minutes"};
+	private JComboBox<String> d = new JComboBox<String>();
+	private JLabel songchoice = new JLabel("Song:   ");
+	private JLabel durationchoice = new JLabel("Duration: ");
+	private JButton submit = new JButton("Play  ");
+	private JButton instructions = new JButton("Instructions");
+	public float arrows;
+	protected static URL urla = AnimatedPanel.class.getResource("/resources/Troll.wav");
+	protected static URL urlb = AnimatedPanel.class.getResource("/resources/Hampster.wav");
+	protected static int songarrows;
+	protected static AudioInputStream audioStream;
+protected static Clip audioClip;
+protected static AudioFormat format;
+protected static DataLine.Info info; 
+	 static void play(URL audioFilePath) throws UnsupportedAudioFileException, IOException, LineUnavailableException{	
+		audioStream = AudioSystem.getAudioInputStream(audioFilePath);
+        format = audioStream.getFormat();
+        info = new DataLine.Info(Clip.class, format);
+        	audioClip = (Clip) AudioSystem.getLine(info);
+	            audioClip.open(audioStream);
+	            audioClip.start();  
+	            audioClip.loop(Clip.LOOP_CONTINUOUSLY);
+	    }
+	 public SettingsWindow(){
+		 setVisible(true);
+		 setTitle("Dance Dance Revolution-Main window");
+		 for (int i = 0; i < 2; i++)
+		      c.addItem(songs[i]);
+		
+		 for (int i = 0; i < 3; i++)
+		      d.addItem(Duration[i]);
+		  submit.addActionListener(new ActionListener() {
+		      public void actionPerformed(ActionEvent e) {
+		    		  if(c.getSelectedItem().equals(songs[0])){
+		    			  try {
+							play(urla);
+							
+						} catch (UnsupportedAudioFileException | IOException| LineUnavailableException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
+		    		  }
+		    			if(c.getSelectedItem().equals(songs[1])){
+		    			
+		    				try{
+		    					play(urlb);
+		    				} catch (UnsupportedAudioFileException | IOException| LineUnavailableException e1) {
+								// TODO Auto-generated catch block
+								e1.printStackTrace();
+							}
+		    			}
+		    		
+		    		  if(d.getSelectedIndex()==0){
+		    			  songarrows=1;
+		    		  }
+		    		  if(d.getSelectedIndex()==1){
+		    			  songarrows=56;
+		    		  }
+		    		  if(d.getSelectedIndex()==2){
+		    			  songarrows=84;
+		    		  }
+		    		  try {
+		    			 
+						new GameWindow();
+					} catch (IOException e1) {
+						
+						e1.printStackTrace();
+					}
+		    		  setVisible(false);
+				
+		      }
+		    });
+		  instructions.addActionListener(new ActionListener() {
+		      public void actionPerformed(ActionEvent e) {
+		    		 instructionsWindow();
+				
+		      }
+		    });
+		 
+		 Container cp = getContentPane();
+		    cp.setLayout(new FlowLayout());
+		cp.add(songchoice);
+		    cp.add(c);
+		    cp.add(durationchoice);
+		cp.add(d);
+		cp.add(instructions);
+		cp.add(submit);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(210,300);
+	setResizable(false);
+		    }
+		public static void instructionsWindow(){
+		
+			JFrame instructions = new JFrame("INSTRUCTIONS");
+			Container content = instructions.getContentPane();
+			content.setLayout(new GridLayout(10, 10));
+			
+			Label label = new Label("------------------------INSTRUCTIONS------------------------");
+			label.setFont(new Font("Serif", Font.PLAIN, 20));
+			Label label2 = new Label("When an arrow gets in the red box in the bottom left hand corner");
+			label2.setFont(new Font("Serif", Font.PLAIN, 15));
+			Label label3 = new Label("Press the arrow on your keyboard that corresponds with the arrow in the box");
+			label3.setFont(new Font("Serif", Font.PLAIN, 15));
+			Label label4 = new Label("Pressing arrows before an arrow is in the box will deduct you 5 points");
+			label4.setFont(new Font("Serif", Font.PLAIN, 15));
+			Label label5 = new Label("A miss will deduct you 5 points");
+			label5.setFont(new Font("Serif", Font.PLAIN, 15));
+			Label label6 = new Label("A hit will grant you 5 points");
+			label6.setFont(new Font("Serif", Font.PLAIN, 15));
+			
+			content.add(label);
+			content.add(label2);
+			content.add(label3);
+			content.add(label4);
+			content.add(label5);
+			content.add(label6);
+
+			instructions.setSize(500, 200);
+			instructions.setResizable(false);
+			instructions.setVisible(true);
+		}
+	
+}
+
+
